796ca7a8e2bc3254a2e680c760d30d0e45a96c71
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2016-11-14 12:19:34 +0000

    Adds existing codebase

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # courtauld-screen-options
-This WordPress plugin sets the default 'Screen options' for all new users. This is designed to help new users ease into using WordPress, and to avoid having to get them to individually set their own preferences. This works independently of the installed theme.
+
+This WordPress plugin sets the default 'Screen options' for all new users.
+
+When any new user is registered this plugin runs and sets their default viewing options.
+
+This way we don't have to worry about Screen Options and dragging and dropping widgets around.
+
+This is designed to help new users ease into using WordPress, and to avoid having to get them to individually set their own preferences.
+
+This works independently of the installed theme.
---
+++ courtauld-screen-options.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+   Plugin Name: Default Screen Options
+   Description: When any new user is registered this plugin runs and sets their default viewing options. This way we don't have to worry about Screen Options and dragging and dropping widgets around.
+   Version: 1.0
+   Author: Jacob Charles Wilson
+   Author URI: https://jclwilson.github.io
+   License: GPL2
+   */
+
+function set_user_metaboxes($user_id) {
+
+    // order
+    $meta_key = 'meta-box-order_post';
+    $meta_value = array(
+        'side' => 'submitdiv,formatdiv,categorydiv',
+        'normal' => 'postexcerpt,postimagediv,trackbacksdiv,tagsdiv-post_tag,postcustom,commentstatusdiv,commentsdiv,slugdiv,authordiv',
+        'advanced' => 'revisionsdiv',
+    );
+    update_user_meta( $user_id, $meta_key, $meta_value );
+
+    // hiddens
+    $meta_key = 'metaboxhidden_post';
+    $meta_value = array('trackbacksdiv','commentstatusdiv','commentsdiv','slugdiv','authordiv');
+    update_user_meta( $user_id, $meta_key, $meta_value );
+
+}
+add_action('user_register', 'set_user_metaboxes');
+
+?>
